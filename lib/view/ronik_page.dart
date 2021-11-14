261684607e321216df953bf09af3aeb4d0019a45
dRoTDM <ironikxd@gmail.com>
2021-11-14 21:23:55 +0545

    Ronik app layout

--- lib/view/ronik_page.dart
+++ lib/view/ronik_page.dart
@@ -6,142 +6,81 @@ class RonikPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Container(
-                child: const Center(
-                    child: Text(
-                  'box01',
-                  style: TextStyle(color: Colors.white, fontSize: 20),
-                )),
-                height: 200,
-                width: 110,
-                decoration: const BoxDecoration(
-                  color: Colors.brown,
-                  shape: BoxShape.circle,
-                ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              height: 150,
+              width: 110,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.brown,
               ),
-              Container(
-                child: const Center(
-                  child: Text(
-                    'box02',
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
-                ),
-                height: 200,
-                width: 110,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.red,
-                ),
-              ),
-              Container(
-                  child: const Center(
-                    child: Text(
-                      'box03',
-                      style: TextStyle(color: Colors.white, fontSize: 20),
-                    ),
-                  ),
-                  height: 200,
-                  width: 110,
-                  decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.pink,
-                  )),
-            ],
-          ),
-          Row(
-            children: [
-              Container(
-                child: const Center(
-                  child: Text(
-                    'box04',
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
-                ),
-                height: 200,
-                width: 120,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.blue,
-                ),
-              ),
-              Container(
-                child: const Center(
-                  child: Text(
-                    'box05',
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
-                ),
-                height: 200,
-                width: 120,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.teal,
-                ),
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Container(
-                height: 200,
-                width: 100,
-                child: const Center(
-                  child: Text(
-                    'box06',
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
-                ),
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.purple,
-                ),
-              ),
-              Container(
-                height: 200,
-                width: 110,
-                child: const Center(
-                  child: Text(
-                    'box07',
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
-                ),
-                decoration: const BoxDecoration(
-                    shape: BoxShape.circle, color: Colors.red),
-              ),
-              Container(
-                child: const Center(
-                  child: Text(
-                    'box08',
-                    style: TextStyle(color: Colors.white, fontSize: 20),
-                  ),
-                ),
-                height: 200,
-                width: 100,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.blue,
-                ),
-              ),
-              Container(
-                  child: const Center(
-                    child: Text(
-                      'box09',
-                      style: TextStyle(color: Colors.white, fontSize: 14),
-                    ),
-                  ),
-                  height: 200,
-                  width: 50,
-                  decoration: const BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.orange,
-                  )),
-            ],
-          ),
-        ],
+            ),
+            const Text(
+              'Ronik Karki',
+              style: TextStyle(color: Colors.grey),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+
+            Container(
+              height: 50,
+              width: 330,
+              color: Colors.greenAccent,
+              child: Center(
+                  child: const Text(
+                "Add Friend",
+                style: TextStyle(color: Colors.white),
+              )),
+            ),
+            //Space between boxes
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 50,
+              width: 330,
+              color: Colors.greenAccent,
+              child: Center(
+                  child: const Text(
+                "Block",
+                style: TextStyle(color: Colors.white),
+              )),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 50,
+              width: 330,
+              color: Colors.greenAccent,
+              child: Center(
+                  child: const Text(
+                "Report",
+                style: TextStyle(color: Colors.white),
+              )),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Container(
+              height: 50,
+              width: 330,
+              color: Colors.greenAccent,
+              child: Center(
+                  child: const Text(
+                "Like",
+                style: TextStyle(color: Colors.white),
+              )),
+            ),
+            const SizedBox(
+              height: 175,
+            ),
+            const Text("Ronik app 2021"),
+          ],
+        ),
       ),
       appBar: AppBar(
         title: Text('Appbar'),
